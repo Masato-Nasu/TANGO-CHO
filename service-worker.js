16a737e37e69fac0a1bc25e2669a4ae5eb70fb2e
那須　雅人 <1bitexist@gmail.com>
2026-02-01 19:07:28 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 /* TANGO-CHO Service Worker (stable updates)
- * Build: v47.1.3
+ * Build: v47.1.4
  */
-const CACHE_NAME = 'tango-cho-cache-v47.1.3';
+const CACHE_NAME = 'tango-cho-cache-v47.1.4';
 
 const CORE_ASSETS = [
   "./",
